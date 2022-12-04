c3bd43424b163fad9a14437573080a5905795793
Stanislaw <val_zur@mail.ru>
2022-12-04 20:46:52 +0300

    Added ML and some classes to model

---
+++ HeartRateMonitor/Model/ModelInput.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartRateMonitor.Model
+{
+    public class ModelInput
+    {
+        [LoadColumn(0)]
+        public float Rate { get; set; }
+    }
+}
---
+++ HeartRateMonitor/Model/ModelOutput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeartRateMonitor.Model
+{
+    public class ModelOutput
+    {
+        public float[] RateFuture { get; set; }
+    }
+}
